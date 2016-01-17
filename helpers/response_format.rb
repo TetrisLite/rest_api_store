50d4f28fa0581b298f47c040a8c997ea7a6c692d
Ade Oke <oke.ade@gmail.com>
2016-01-17 14:15:25 +0000

    added some get routes

---
+++ helpers/response_format.rb
@@ -0,0 +1,15 @@
+require "sinatra/base"
+
+module Sinatra
+  module ResponseFormat
+    def format_response(data, accept)
+      accept.each do |type|
+        return data.to_xml if type.downcase.eql? "text/xml"
+        return data.to_json if type.downcase.eql? "application/json"
+        return data.to_json
+      end
+    end
+  end
+  #this will enhance sinatras methods with our own defined methods
+  helpers(ResponseFormat)
+end
